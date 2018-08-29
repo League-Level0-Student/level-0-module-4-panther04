d1355d0b64786af7233fc43288edea2a0de82961
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-28 19:24:09 -0700

    adventure

---
+++ src/Adventure.java
@@ -0,0 +1,66 @@
+import javax.swing.JOptionPane;
+
+public class Adventure {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"You are on a island and your friend is in a ditch but you found a Map \n FULL of gold what are going to do A. Save \n your Best friend that you said you are not going to leave behind B.Find the gold and share it and not save your friend");
+int first =Integer.parseInt( JOptionPane.showInputDialog("0 to save your friend or 1 do go  after the gold"));
+	if (first==0) {
+		JOptionPane.showMessageDialog(null,"Your friend is out of the ditch but the map flew away to a jungle stuck on a brach \n but you are surrondid by sharks A. you and your friend swimm to the island or B. try to build a boat (you have a axe)");
+	}
+	if (first==1) {
+		JOptionPane.showMessageDialog(null,"you see a Jungle not to far so you build a bout for two people and the treasher map \n loot is on the Jungle");
+	}
+	JOptionPane.showMessageDialog(null,"you are on the jungle but you see a jaguar A. do you youse your axe to kill it or B.you run away far form it");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
